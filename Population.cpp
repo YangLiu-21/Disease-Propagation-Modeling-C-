cae495905e3ce55c253e68833549442445f313b8
Yang Liu <59941142+YangLiu-21@users.noreply.github.com>
2022-10-19 00:08:55 -0500

    Update Population.cpp

--- Population.cpp
+++ Population.cpp
@@ -1,11 +1,6 @@
 class Population
 {
-      private:
-              int n, v, i, npeople, nvaccinate;
-              vector<Person> people;
-      public:
-              Population(){};
-              //initialization
+              . . . . . . 
               Population(int people_number, int nday, float percentage)
                  {
                     n = nday;
@@ -23,61 +18,66 @@ class Population
                           i++;
                        };
                     };
-                    cout << v << people[v].status_string() << endl;
+                  //  cout << v << people[v].status_string() << endl;
                  };
-              void random_infection()
-                 {
-                     for (int i=0; ; i++){
-                     v = rand() % npeople;
-                     people[v].infect(n);
-                     if (people[v].get_state()==1){
-                         break;};
-                     }
-                    // cout << v <<people[v].status_string() << endl;
-                 };
-              int count_infected()
-                 {
-                     int n_infected = 0;
-                     for (int i=0; i < npeople; i++){
-                          if (people[i].get_state()==1){
-                          n_infected = n_infected + 1;};
-                   // cout << people[i].get_state() << endl;
-                     };
-                     return n_infected;
-                 };
-              void update()
-                 {
-                     for (int i=0; i < npeople; i++){
-                          people[i].update();
-                     };
-                 };
-              bool is_stable(){
-                     int flag = 0;
-                     for (int i=0; i < npeople; i++){
-                          if (people[i].get_state()== -1 || people[i].get_state()== 0 || people[i].get_state()== -2 )
-                          { continue;}
-                          else
-                          {flag = 1;}
-                     };
-                     return  flag == 0;
-                 };
-              string displays(){
-                     string dis = "";
-                     string s = "";
-                   for (int i=0; i < npeople; i++){
-                        switch (people[i].get_state()) {
-                             case 0:
-                                s = "?";
-                                break;
-                             case -1:
-                                s = "-";
-                                break;
-                             case -2:
-                                s = "v";
-                                break;
-                             case 1:
-                                s = "+";};
-                         dis = dis + s;
-                        };
-                       return dis;
                    };
+                   
+              void set_probability_of_transfer(float probability){
+                           int neighbor1, neighbor2;
+                           int lower_bound, upper_bound;
+                           lower_bound = 0;
+                           upper_bound = npeople-1;
+                           vector<int> direct_neighbors;
+                           for (int i=0; i < npeople; i++){
+                               if (people[i].get_state()==1){
+                                      direct_neighbors.push_back(i);
+                                     // cout << i << endl;
+                                     };
+                                 };
+                           if( direct_neighbors.size() >= 0){
+                             //  cout << direct_neighbors[0] << endl;
+                               for (int i=0; i < direct_neighbors.size(); i++){
+                       bool TrueFalse1 = (rand() % 100) < (probability*100);
+                                    if (TrueFalse1) {
+                                            neighbor1 = direct_neighbors[i] + 1;
+                    if(neighbor1 >= lower_bound && neighbor1 <= upper_bound){
+                                               people[neighbor1].infect(n);
+                                            };};
+                     bool TrueFalse2 = (rand() % 100) < (probability*100);
+                                    if (TrueFalse2) {
+                                            neighbor2 = direct_neighbors[i] - 1;
+                    if(neighbor2 >= lower_bound && neighbor2 <= upper_bound){
+                                               people[neighbor2].infect(n);
+                                            };};
+                                };
+                            };
+                        };
+            . . . . . . 
+\end{lstlisting}
+main program:
+\begin{lstlisting}[language=C++ , caption=main program: models a Population]
+              . . . . . . 
+              //Population
+              int npeople = 50; // the  total  number  of the  population
+              int n =5; //with n days to go  before  recovery
+              float probability = 0.5; // the  probability  to get  sick  when  interactswith a sick  person
+              iloat percentage = 0.04; // the  percentage  of  people  that  has  beenvaccinated
+              int ncontact = 10; //the  number  of  people  that a person  comes  intocontact  with
+
+              Population population(npeople, n, percentage);
+              population.random_infection();
+              cout << "Size of" << endl;
+              cout << "population?" << endl;
+              cout << npeople << " people" << endl;
+             // population.random_interactions(probability,ncontact);
+              for (int step = 1; ; step++ ) {
+                  if (population.is_stable()){
+                  cout <<"Disease ran its course by step"<< step-1<endl;
+                  break;}
+                  population.update();
+                  cout << "In Step " << step << " #sick: " <<population.count_infected()
+                       << " : " <<  population.displays() << endl;
+                  population.set_probability_of_transfer(probability);
+                 // population.random_interactions(probability,ncontact);
+
+              };
